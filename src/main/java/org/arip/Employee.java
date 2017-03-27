1d146b713c36c7e3fe45cd7be6274fedb981aed9
Arip Hidayat <ariphidayat@hotmail.com>
2017-03-27 17:00:49 +0700

    Conditional Operations

--- src/main/java/org/arip/Employee.java
+++ src/main/java/org/arip/Employee.java
@@ -1,10 +1,12 @@
 package org.arip;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Version;
 
 /**
  * Created by Arip Hidayat on 3/21/2017.
@@ -17,6 +19,7 @@ public class Employee {
     private String firstName;
     private String lastName;
     private String description;
+    private @Version @JsonIgnore Long version;
 
     private Employee() {}
 
